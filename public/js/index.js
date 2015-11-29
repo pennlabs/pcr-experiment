a814db19a7d6d118c058d4e13cd561d37d4fa8f0
Adel Qalieh <aqalieh95@gmail.com>
2015-11-29 13:03:14 -0500

    Add basic search support

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var courses = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('keywords'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: '/data/courses.json'
+});
+
+$('#bloodhound .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'courses',
+  display: 'title',
+  source: courses
+});
