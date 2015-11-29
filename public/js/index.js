2077ac2f258eeab5f562f88ee061896d90bc5641
Adel Qalieh <aqalieh95@gmail.com>
2015-11-29 13:42:01 -0500

    Select first option by default

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,11 @@ $('#bloodhound .typeahead').typeahead({
   name: 'courses',
   display: 'title',
   source: courses
+}).on('typeahead:render', function (e) {
+  $('#bloodhound').find('.tt-selectable:first').addClass('tt-cursor');
+  $('#bloodhound').find('.tt-selectable').on('mouseover', function (e) {
+    $('#bloodhound').find('.tt-selectable:first').removeClass('tt-cursor');
+  });
 }).on('typeahead:selected', function (e, datum) {
   document.location.href += datum.url;
 });
